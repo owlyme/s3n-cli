ea0174b56f4d257ea4275c82ad43af14e523f3ef
owlyme <1148452777@qq.com>
2020-12-24 21:05:24 +0800

    ..

--- public/eslint/.eslintrc.js
+++ public/eslint/.eslintrc.js
@@ -12,8 +12,17 @@ module.exports = {
   rules: {
     indent: ['error', 2],
     quotes: ['error', 'single'],
-    // semi: ['error', 'always'],
-    'no-console': 0
+    semi: ['error', 'always'],
+    "no-multiple-empty-lines": [1, { "max": 1 }],
+    'no-console': 0,
+    "no-trailing-spaces": 1,
+    eqeqeq: 2,
+    "no-empty": 2,
+    "no-extra-semi": 2,
+    "no-extra-parens": 2,
+    "no-irregular-whitespace": 2,
+    "no-lone-blocks": 2,
+    "no-lonely-if": 2,
   },
   globals: {
     getApp: false,
